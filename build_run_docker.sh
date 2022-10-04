638dfa6133d98ff3a4c731e3463a7d92a6ff2852
YuxuanLiu-kayla <yuxuan410liu@gmail.com>
2022-10-03 21:55:26 -0400

    Modify tfrecord data path

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -7,8 +7,7 @@ if [[ "$mode" == "train" ]]
 then
     sudo docker run --runtime=nvidia -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --rm --ipc=host \
     -v ${PWD}/data:/data -v ${PWD}/results:/results -v /raid/data/imseg/preproc-data:/preproc-data \
-    unet3d_tf:test_ry /bin/bash scripts/unet3d_train_single.sh 1 \
-    ../DeepLearningExamples/TensorFlow/Segmentation/UNet_3D_Medical/data /results 2
+    unet3d_tf:test_ry /bin/bash scripts/unet3d_train_single.sh 1 /raid/data/imseg/tfrecord-data /results 2
 elif [[ "$mode" == "container" ]]
 then
     sudo docker run --runtime=nvidia -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --rm --ipc=host \
