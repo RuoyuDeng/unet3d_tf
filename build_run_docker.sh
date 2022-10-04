e55a51915ba397b985cca8243753f1c325bbc389
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-03 22:05:38 -0400

    update build_run_docker script to raid data

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-sudo docker build -t unet3d_tf:test_ry .
+sudo docker build -t unet3d_tf:test .
 mode=$1
+
+result_path="/raid/data/imseg/results"
+tf_data_path="/raid/data/imseg/tfrecord-data"
+npy_data_path="/raid/data/imseg/preproc-data"
 
 if [[ "$mode" == "train" ]]
 then
     sudo docker run --runtime=nvidia -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --rm --ipc=host \
-    -v ${PWD}/data:/data -v ${PWD}/results:/results -v /raid/data/imseg/preproc-data:/preproc-data \
-    unet3d_tf:test_ry /bin/bash scripts/unet3d_train_single.sh 1 /data /results 2
+    -v ${tf_data_path}:/data -v ${result_path}:/results -v ${npy_data_path}:/preproc-data \
+    unet3d_tf:test /bin/bash scripts/unet3d_train_single.sh 1 /data /results 2
 elif [[ "$mode" == "container" ]]
 then
     sudo docker run --runtime=nvidia -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --rm --ipc=host \
-    -v ${PWD}/data:/data -v ${PWD}/results:/results -v /raid/data/unet/unet3d_tf/npy_data:/preproc-data \
-    unet3d_tf:test_ry /bin/bash
+    -v ${tf_data_path}:/data -v ${result_path}:/results -v ${npy_data_path}:/preproc-data \
+    unet3d_tf:test /bin/bash
 fi
 
