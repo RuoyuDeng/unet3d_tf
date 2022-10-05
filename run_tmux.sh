4d4667315d117df1913e3bf2971af6c426541606
YuxuanLiu-kayla <yuxuan410liu@gmail.com>
2022-10-05 11:22:22 -0400

    Modify content of run_tmux.sh

--- run_tmux.sh
+++ run_tmux.sh
@@ -2,4 +2,4 @@
 tmux kill-session -t unet3d_tf 2>/dev/null
 # Start a new tmux session from which we will run training
 tmux new-session -d -s unet3d_tf
-tmux send-keys -t dlio_unet "./build_run_docker.sh train" C-m
+tmux send-keys -t unet3d_tf "./build_run_docker.sh train" C-m
