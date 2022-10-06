408a5888f298e0221d3f009076826d96f4c8a5c4
Ruoyu <ruoyu.deng@mail.mcgill.ca>
2022-10-06 11:38:34 -0400

    tiny update to remove redundant docker path

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -4,18 +4,17 @@ sudo docker build -t unet3d_tf:test .
 mode=$1
 
 result_path="/raid/data/imseg/results"
-tf_data_path="/raid/data/imseg/tfrecord-data"
-npy_data_path="/raid/data/imseg/preproc-data"
+tf_data_path="/raid/data/imseg/29gb-tf"
 
 if [[ "$mode" == "train" ]]
 then
     sudo docker run --runtime=nvidia -it  --rm --ipc=host \
-    -v ${tf_data_path}:/data -v ${result_path}:/results -v ${npy_data_path}:/preproc-data \
+    -v ${tf_data_path}:/data -v ${result_path}:/results \
     unet3d_tf:test /bin/bash scripts/unet3d_train_single.sh 1 /data /results 1
 elif [[ "$mode" == "container" ]]
 then
-    sudo docker run --runtime=nvidia -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --rm --ipc=host \
-    -v ${tf_data_path}:/data -v ${result_path}:/results -v ${npy_data_path}:/preproc-data \
+    sudo docker run --runtime=nvidia -it  --rm --ipc=host \
+    -v ${tf_data_path}:/data -v ${result_path}:/results \
     unet3d_tf:test /bin/bash
 fi
 
--- prepro.sh
+++ prepro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 raw_npy_dir="/raid/data/imseg/29gb-npy"
-tf_dir="/raid/data/imseg/29gb-test"
+tf_dir="/raid/data/imseg/29gb-tf"
 python3 dataset/prepro.py --np-raw ${raw_npy_dir} --tf-data ${tf_dir} --do-crop --do-reshape
